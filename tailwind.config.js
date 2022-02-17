34a1a5859a4236df6b03aa7715de464c3822264c
Yaman KATBY <m.yaman.katby@gmail.com>
2022-02-17 17:14:17 +0300

    ➕ Add `TailwindCSS` and configure it

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
