c305261b467d6a12c7e888dc95d7d033b19ae6ec
Yaman KATBY <m.yaman.katby@gmail.com>
2022-02-18 15:11:30 +0300

    ➕ Add `remark-prism` and configure it

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['tsx', 'ts', 'js', 'jsx', 'md', 'mdx'],
+  pageExtensions: ['tsx', 'ts', 'js', 'jsx', 'md', 'mdx']
 }
 
 const withMDX = require('@next/mdx')({
-  extension: /\.(md|mdx)$/,
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [require('remark-prism')]
+  }
 })
 
 module.exports = withMDX(nextConfig)
