b4119e90b8fe2f6e92f64be26e93b581a0b1e64c
Yaman KATBY <m.yaman.katby@gmail.com>
2022-02-21 18:59:06 +0300

    🔎 Generate sitemap.xml

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://rn-material.js.org/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
