d39cb2da83ec953efe7c4bf9bb29e4c6641d59f2
Yaman KATBY <m.yaman.katby@gmail.com>
2022-02-21 19:07:30 +0300

    🔎 Add GTAG script

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import React, { PropsWithChildren } from "react";
+import Script from "next/script";
 import Head from "next/head";
 import Sidebar, { ISidebar, ISidebarItem } from "./Sidebar";
 
@@ -12,6 +13,17 @@ interface IProps {
 const Layout: React.FC<IProps> = ({ sidebar, current, children }) => {
   return (
     <div className="min-h-screen flex flex-col lg:flex-row">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-PTGCSNV1RJ" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PTGCSNV1RJ');
+        `}
+      </Script>
+
       <Head>
         <title>{current.title} | React Native Material</title>
         <meta name="description" content={current.description} />
