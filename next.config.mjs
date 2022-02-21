3535df2018dc847dfcaa1701e13ba6657d9c3890
Yaman KATBY <m.yaman.katby@gmail.com>
2022-02-21 18:45:38 +0300

    🎨 Format code

--- next.config.mjs
+++ next.config.mjs
@@ -74,10 +74,10 @@ const nextConfig = {
 };
 
 const remarkLayout = () => (tree, file) => {
-  if (tree.children[0].type === 'thematicBreak') {
-    const firstHeadingIndex = tree.children.findIndex(t => t.type === 'heading')
+  if (tree.children[0].type === "thematicBreak") {
+    const firstHeadingIndex = tree.children.findIndex((t) => t.type === "heading");
     if (firstHeadingIndex !== -1) {
-      tree.children.splice(0, firstHeadingIndex + 1)
+      tree.children.splice(0, firstHeadingIndex + 1);
     }
   }
 
